16493be1bf9eddbf111de492d7c4eeb06e82ae0f
Lucas Dias <ldias.psico@gmail.com>
2022-01-21 16:35:49 -0300

    ajustes footer

--- footer.php
+++ footer.php
@@ -22,12 +22,15 @@
                     <h3>Atendimento</h3>
                     <ul class="contato">
                         <li>Loja 01</li>
+                        <br>
                         <li> <img src="images/icon-pin.svg" alt="Ícone localização"> Av. Dep. Dante Delmanto, 1895<br>Vila Paulista | Botucatu - SP  <span class="mapa"><a href="https://www.google.com.br/maps/dir/''/marcenariamiqueas/@-22.8691553,-48.5000779,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x94c720b971837e15:0x5b21f72f3015d457!2m2!1d-48.465745!2d-22.869159" target="_blank"> [ mapa ]</a> </span></li>
                         <li><img src="images/icon-phone.svg" alt="Ícone telefone"> 14 3815.1602</li>
                         <li><img src="images/icon-email.svg" alt="Ícone e-mail"> contato@marcenariamiqueas.com.br</li>
                     </ul>
+                    <br>
                     <ul class="contato">
                         <li>Loja 02</li>
+                        <br>
                         <li><img src="images/icon-pin.svg" alt="Ícone localização"> Avenida Marginal Duzentos, 1050<br>Vila Real | Botucatu - SP  <span class="mapa"><a href="https://www.google.com/maps/place/Shopping+Park+Botucatu/@-22.9148728,-48.4638933,17.25z/data=!4m5!3m4!1s0x94c6df23759d49bf:0xa61da4e8682a0cf4!8m2!3d-22.9148397!4d-48.4639549" target="_blank"> [ mapa ]</a> </span></li>
                         <li><img src="images/icon-phone.svg" alt="Ícone telefone"> 14 3815.1602</li>
                         <li><img src="images/icon-email.svg" alt="Ícone e-mail"> contato@marcenariamiqueas.com.br</li>
